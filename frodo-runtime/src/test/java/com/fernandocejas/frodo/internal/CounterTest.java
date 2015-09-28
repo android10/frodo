6b40923b1aa4ec297075d39639d9c456051af2f0
Fernando Cejas <fcejas@gmail.com>
2015-09-28 23:40:39 +0200

    Add tests for counter class.

---
+++ frodo-runtime/src/test/java/com/fernandocejas/frodo/internal/CounterTest.java
@@ -0,0 +1,73 @@
+package com.fernandocejas.frodo.internal;
+
+import com.fernandocejas.frodo.JavaUnitTest;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CounterTest extends JavaUnitTest {
+
+  private static final String COUNTER_NAME = "android10";
+
+  private Counter counter;
+
+  @Before
+  public void setUp() {
+    counter = new Counter(COUNTER_NAME);
+  }
+
+  @Test
+  public void incrementsCounter() {
+    counter.increment();
+    counter.increment();
+    counter.increment();
+
+    assertThat(counter.tally()).isEqualTo(3);
+  }
+
+  @Test
+  public void clearsCounter() {
+    counter.increment();
+    counter.increment();
+    assertThat(counter.tally()).isEqualTo(2);
+
+    counter.clear();
+    assertThat(counter.tally()).isZero();
+  }
+
+  @Test
+  public void decrementsCounter() {
+    counter.increment();
+    counter.increment();
+    counter.increment();
+    counter.decrement();
+    counter.decrement();
+
+    assertThat(counter.tally()).isEqualTo(1);
+  }
+
+  @Test
+  public void comparesCounterValues() {
+    final Counter anotherCounter = new Counter("myCounter");
+
+    counter.increment();
+    counter.increment();
+    assertThat(counter.compareTo(anotherCounter)).isEqualTo(1);
+
+    anotherCounter.increment();
+    anotherCounter.increment();
+    assertThat(counter.compareTo(anotherCounter)).isZero();
+
+    counter.decrement();
+    assertThat(counter.compareTo(anotherCounter)).isEqualTo(-1);
+  }
+
+  @Test
+  public void countsNegatives() {
+    counter.decrement();
+    counter.decrement();
+
+    assertThat(counter.tally()).isEqualTo(-2);
+  }
+}
