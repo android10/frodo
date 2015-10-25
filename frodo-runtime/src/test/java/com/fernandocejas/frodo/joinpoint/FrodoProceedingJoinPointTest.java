410639abc1e31e2a6a187be5014431abbaa0173c
Fernando Cejas <fcejas@gmail.com>
2015-10-25 21:29:52 +0100

    Add tests for FrodoProceedingJoinPoint.

---
+++ frodo-runtime/src/test/java/com/fernandocejas/frodo/joinpoint/FrodoProceedingJoinPointTest.java
@@ -0,0 +1,38 @@
+package com.fernandocejas.frodo.joinpoint;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FrodoProceedingJoinPointTest {
+
+  private FrodoProceedingJoinPoint frodoJoinPoint;
+
+  private TestJoinPoint joinPoint;
+  private TestProceedingJoinPoint proceedingJoinPoint;
+
+  @Before
+  public void setUp() throws Exception {
+    joinPoint = new TestJoinPoint.Builder(this.getClass()).build();
+    proceedingJoinPoint = new TestProceedingJoinPoint(joinPoint);
+    frodoJoinPoint = new FrodoProceedingJoinPoint(proceedingJoinPoint);
+  }
+
+  @Test
+  public void shouldDelegateProceedMethod() throws Throwable {
+    frodoJoinPoint.proceed();
+
+    proceedingJoinPoint.assertProceedMethodCalled();
+  }
+
+  @Test
+  public void shouldDelegateProceedMethodWithArgs() throws Throwable {
+    Object[] args = new Object[] { 1, 2, 3 };
+
+    frodoJoinPoint.proceed(args);
+
+    proceedingJoinPoint.assertProceedMethodCalledWithArgs(args);
+  }
+}
