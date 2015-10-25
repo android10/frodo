65ab5f0fc63c64af49104f5e1639ab13151cabe7
Fernando Cejas <fcejas@gmail.com>
2015-10-25 18:32:18 +0100

    Add implementation for TestJoinPoint.

--- frodo-runtime/src/test/java/com/fernandocejas/frodo/joinpoint/TestJoinPoint.java
+++ frodo-runtime/src/test/java/com/fernandocejas/frodo/joinpoint/TestJoinPoint.java
@@ -1,36 +1,64 @@
 package com.fernandocejas.frodo.joinpoint;
 
+import java.lang.reflect.Method;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.Signature;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.aspectj.lang.reflect.SourceLocation;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.BDDMockito.given;
 
 class TestJoinPoint implements JoinPoint {
 
-  TestJoinPoint() {
+  @Mock private MethodSignature methodSignature;
+  @Mock private Method method;
+
+  private final Class declaringType;
+  private final String methodName;
+  private final Class[] methodParameterTypes;
+  private final String[] methodParameterNames;
+  private final Object[] methodParameterValues;
+
+  private TestJoinPoint(JoinPointBuilder builder) {
+    this.declaringType = builder.declaringType;
+    this.methodName = builder.methodName;
+    this.methodParameterTypes = builder.methodParameterTypes;
+    this.methodParameterNames = builder.methodParameterNames;
+    this.methodParameterValues = builder.methodParameterValues;
+
+    MockitoAnnotations.initMocks(this);
+    given(method.getGenericReturnType()).willReturn(declaringType);
+    given(methodSignature.getDeclaringType()).willReturn(declaringType);
+    given(methodSignature.getName()).willReturn(methodName);
+    given(methodSignature.getParameterTypes()).willReturn(methodParameterTypes);
+    given(methodSignature.getParameterNames()).willReturn(methodParameterNames);
+    given(methodSignature.getMethod()).willReturn(method);
   }
 
   @Override public String toShortString() {
-    return null;
+    return methodName;
   }
 
   @Override public String toLongString() {
-    return null;
+    return declaringType.getSimpleName() + "#" + methodName;
   }
 
   @Override public Object getThis() {
-    return null;
+    return this;
   }
 
   @Override public Object getTarget() {
-    return null;
+    return methodName;
   }
 
   @Override public Object[] getArgs() {
-    return new Object[0];
+    return methodParameterValues;
   }
 
   @Override public Signature getSignature() {
-    return null;
+    return methodSignature;
   }
 
   @Override public SourceLocation getSourceLocation() {
@@ -43,5 +71,38 @@ class TestJoinPoint implements JoinPoint {
 
   @Override public StaticPart getStaticPart() {
     return null;
+  }
+
+  static class JoinPointBuilder {
+    private final Class declaringType;
+    private final String methodName;
+
+    private Class[] methodParameterTypes;
+    private String[] methodParameterNames;
+    private Object[] methodParameterValues;
+
+    JoinPointBuilder(Class declaringType, String methodName) {
+      this.declaringType = declaringType;
+      this.methodName = methodName;
+    }
+
+    JoinPointBuilder withParamTypes(Class... paramTypes) {
+      this.methodParameterTypes = paramTypes;
+      return this;
+    }
+
+    JoinPointBuilder withParamNames(String... paramNames) {
+      this.methodParameterNames = paramNames;
+      return this;
+    }
+
+    JoinPointBuilder withParamValues(Object... paramValues) {
+      this.methodParameterValues = paramValues;
+      return this;
+    }
+
+    TestJoinPoint build() {
+      return new TestJoinPoint(this);
+    }
   }
 }
