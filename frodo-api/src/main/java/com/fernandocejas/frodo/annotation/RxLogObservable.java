9912768d3c56fb048deed451e540671f17640af4
Fernando Cejas <fcejas@gmail.com>
2015-11-30 00:54:06 +0100

    Update javadoc and added Scope logging options for Observables and Subscribers.

--- frodo-api/src/main/java/com/fernandocejas/frodo/annotation/RxLogObservable.java
+++ frodo-api/src/main/java/com/fernandocejas/frodo/annotation/RxLogObservable.java
@@ -8,23 +8,36 @@ import static java.lang.annotation.RetentionPolicy.CLASS;
 
 /**
  * <br>Annotated methods which return rx.Observables will print the following information on
- * android logcat when emitting items.<br>
+ * android logcat when emitting items.
+ * A {@link Scope} option can be passed to choose different logging scopes.<br>
  *
  * <br>OUTPUT EXAMPLE:<br>
  *
  * <br>Frodo => [@Observable :: @InClass -> ObservableSample :: @Method -> names()]
- * <br>Frodo => [@Observable#names -> onSubscribe() :: @SubscribeOn -> RxNewThreadScheduler-8]
+ * <br>Frodo => [@Observable#names -> onSubscribe()]
  * <br>Frodo => [@Observable#names -> onNext() -> Fernando]
  * <br>Frodo => [@Observable#names -> onNext() -> Silvia]
  * <br>Frodo => [@Observable#names -> onCompleted()]
- * <br>Frodo => [@Observable#names -> onTerminate() :: @Emitted -> 2 elements :: @Time -> 1 ms]
+ * <br>Frodo => [@Observable#names -> onTerminate()]
+ * <br>Frodo => [@Observable#names -> @Emitted -> 2 elements :: @Time -> 1 ms]
+ * <br>Frodo => [@Observable#names -> @SubscribeOn -> RxNewThreadScheduler-8 :: @ObserveOn -> main]
  * <br>Frodo => [@Observable#names -> onUnsubscribe()]<br>
  *
- * <br>------------------------------------------------------------------------------------------
- * <br>"I am glad you are here with me. Here at the end of all things, Sam." -Frodo
- * <br>------------------------------------------------------------------------------------------
+ * @see <a href="https://github.com/android10/frodo/wiki">Frodo Documentation</a>
  */
 @Retention(CLASS)
 @Target({ METHOD })
 public @interface RxLogObservable {
+  Scope value() default Scope.EVERYTHING;
+
+  /**
+   * Logging scope of the current annotated rx.Observable.<br>
+   *
+   * <li>{@link #EVERYTHING}: Logs stream data, schedulers and rx.Observable events. Default.</li>
+   * <li>{@link #STREAM}: Logs rx.Observable emitted items plus total execution time.</li>
+   * <li>{@link #SCHEDULERS}: Logs schedulers where the annotated rx.Observable operates on.</li>
+   * <li>{@link #EVENTS}: Logs rx.Observable events only.</li>
+   * <li>{@link #NOTHING}: Turns off logging for the annotated rx.Observable.</li>
+   */
+  enum Scope { EVERYTHING, STREAM, SCHEDULERS, EVENTS, NOTHING }
 }
--- frodo-api/src/main/java/com/fernandocejas/frodo/annotation/RxLogSubscriber.java
+++ frodo-api/src/main/java/com/fernandocejas/frodo/annotation/RxLogSubscriber.java
@@ -8,7 +8,8 @@ import static java.lang.annotation.RetentionPolicy.CLASS;
 
 /**
  * <br>Annotated classes which are of type rx.Subscriber will print the following information on
- * android logcat when receiving items from an rx.Observable:<br>
+ * android logcat when receiving items from an rx.Observable.
+ * A {@link Scope} option can be passed to choose different logging scopes.<br>
  *
  * <br>OUTPUT EXAMPLE:<br>
  *
@@ -17,14 +18,25 @@ import static java.lang.annotation.RetentionPolicy.CLASS;
  * <br>Frodo => [@Subscriber :: MySubscriberBackpressure -> onNext() -> 1 :: @ObserveOn -> main]
  * <br>Frodo => [@Subscriber :: MySubscriberBackpressure -> onNext() -> 2 :: @ObserveOn -> main]
  * <br>Frodo => [@Subscriber :: MySubscriberBackpressure -> onNext() -> 3 :: @ObserveOn -> main]
- * <br>Frodo => [@Subscriber :: MySubscriberBackpressure -> onCompleted() :: @Received -> 16 elements :: @Time -> 6 ms]
+ * <br>Frodo => [@Subscriber :: MySubscriberBackpressure -> onCompleted()]
+ * <br>Frodo => [@Subscriber :: MySubscriberBackpressure -> @Received -> 16 elements :: @Time -> 6 ms]
  * <br>Frodo => [@Subscriber :: MySubscriberBackpressure -> unSubscribe()]<br>
  *
- * <br>------------------------------------------------------------------------------------------
- * <br>"I will take the Ring, though I do not know the way." -Frodo
- * <br>------------------------------------------------------------------------------------------
+ * @see <a href="https://github.com/android10/frodo/wiki">Frodo Documentation</a>
  */
 @Retention(CLASS)
 @Target({ TYPE })
 public @interface RxLogSubscriber {
+  Scope value() default Scope.EVERYTHING;
+
+  /**
+   * Logging scope of the current annotated rx.Subscriber.<br>
+   *
+   * <li>{@link #EVERYTHING}: Logs stream data, scheduler and rx.Subscriber events. Default.</li>
+   * <li>{@link #STREAM}: Logs rx.Subscribers received items plus total execution time.</li>
+   * <li>{@link #SCHEDULER}: Logs scheduler where the annotated rx.Subscriber operates on.</li>
+   * <li>{@link #EVENTS}: Logs rx.Subscriber events only.</li>
+   * <li>{@link #NOTHING}: Turns off logging for the annotated rx.Subscriber.</li>
+   */
+  enum Scope { EVERYTHING, STREAM, SCHEDULER, EVENTS, NOTHING }
 }
